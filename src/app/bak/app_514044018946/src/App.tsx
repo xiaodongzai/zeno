5deea18a0f534ab96826b97e0c73f0136bf12edf
xiezhendong1 <xiezhendong1@360.cn>
2025-11-07 22:06:54 +0800

    feat: 移除无用的资源

--- src/app/bak/app_514044018946/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import appRouter from './router';
-
-const App: React.FC = () => {
-  return <RouterProvider router={appRouter} />;
-};
-
-export default App;
